8a9593bec6174c16aea0d4e7f6a2b88fbefc3a60
prathiksharv <prathirv@gmail.com>
2024-08-14 15:52:27 -0400

    changes to human-llm evaluation code

--- evaluation/6_evaluation_human_llm.py
+++ evaluation/6_evaluation_human_llm.py
@@ -225,7 +225,8 @@ def process_annotations(noteid_list, human_folder, llm_folder, output_csv):
 
     for note_id in noteid_list:
         # Construct file paths
-        human_file = os.path.join(human_folder, f"annotations-gpt4o-{note_id}.json")      #change filename format for gpt4o and llama3
+        # human_file = os.path.join(human_folder, f"annotations-gpt4o-{note_id}.json")      #change filename format for gpt4o and llama3
+        human_file = os.path.join(human_folder, f"annotations-llama3-{note_id}.json")
         llm_file = os.path.join(llm_folder, f"{note_id}.json")          #change filename format for gpt4o and llama3
 
         # Load the JSON data
@@ -268,6 +269,8 @@ noteid_list = [
     "10091873-DS-22", "10093120-DS-18", "10097898-DS-11", "10098672-DS-3",
     "10036086-DS-25", "10098875-DS-12"
 ]
+
+#  "10070201-DS-19","10070594-DS-16",  "10076617-DS-11","10078297-DS-5", "10095417-DS-19",  "10036086-DS-25"
 
 # #GPT4o detections
 # human_folder = 'data/human-annotations/set2/gpt4o'  # Replace with the path to your human annotations folder
